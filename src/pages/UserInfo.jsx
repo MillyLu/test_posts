f1f07c9d6480be06d0ee1f56066ae5eda02a0fa9
Julia <mji23@mail.ru>
2023-06-05 22:17:31 +0300

    about

--- src/pages/UserInfo.jsx
+++ src/pages/UserInfo.jsx
@@ -1,11 +1,14 @@
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { fetchUser } from "../store/userReducer";
 import { useEffect } from "react";
 import Card from "react-bootstrap/Card";
 import { PostCard } from "../components/postCard/PostCard";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 import { useNavigate } from "react-router-dom";
+import Spinner from "react-bootstrap/Spinner";
+import { Container } from "react-bootstrap";
+import Badge from 'react-bootstrap/Badge'
 
 export function UserInfoPage() {
   const params = useParams();
@@ -15,6 +18,8 @@ export function UserInfoPage() {
 
   const user = useSelector((state) => state.userReducer.user);
   const comments = useSelector((state) => state.userReducer.comments);
+  const loading = useSelector((state) => state.userReducer.loading);
+
   console.log(user);
   console.log(comments);
 
@@ -24,29 +29,40 @@ export function UserInfoPage() {
 
   return (
     <>
-    <Button onClick={() => navigate(-1)} variant="secondary">Назад</Button>{' '}
-    
+    <Container>
+    <Button onClick={() => navigate(-1)} style={{marginBottom: "30px"}}variant="secondary">
+        Назад
+      </Button>{" "}
+      {loading && (
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      )}
+      <h2>Информация о пользователе <Badge bg="secondary">Info</Badge>:</h2>
       {user.map((userItem) => (
-    <Card style={{ width: "18rem" }}>
-      <Card.Body>
-        <Card.Title>{userItem.username}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">
-          name: {userItem.name}
-        </Card.Subtitle>
-        <Card.Text>website: {userItem.website}</Card.Text>
-      </Card.Body>
-    </Card>
-  ))}
-  {comments &&
-    comments.map((post) => (
-      <PostCard
-        key={post.id}
-        title={post.title}
-        body={post.body}
-        userId={post.userId}
-        postId={post.id}
-      />
-    ))}</>
-  
-  )
+        <Card  style={{ width: "20rem", marginBottom: "20px"}}>
+          <Card.Body>
+            <Card.Title>Nickname: {userItem.username}</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">
+              name: {userItem.name}
+            </Card.Subtitle>
+            <Card.Text >website: {userItem.website}</Card.Text>
+          </Card.Body>
+        </Card>
+      ))}
+      <h3 style={{marginBottom: "20px"}}>Список постов пользователя:</h3>
+      {comments &&
+        comments.map((post) => (
+          <PostCard
+            key={post.id}
+            title={post.title}
+            body={post.body}
+            userId={post.userId}
+            postId={post.id}
+          />
+        ))}
+    </Container>
+     
+    </>
+  );
 }
--- src/saga/user.js
+++ src/saga/user.js
@@ -3,13 +3,29 @@ import { FETCH_USER, setUser,
 FETCH_COMMENTS_BY_USER, setCommentsByUser } from "../store/userReducer";
 import { getUserInfoById, getCommentsByUser } from "../apiServices";
 
+const loading = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(resolve, 500);
+  });
+
+
 function* fetchUserWorker(action) {
+  try {
   const user = yield call(getUserInfoById, action.id);
-  yield put(setUser(user));
   const comments = yield call(getCommentsByUser, action.id);
+  loading()
+  yield put(setUser(user));
   yield put(setCommentsByUser(comments));
+    
+  } catch (error) {
+    
+  }
+
 }
+
+
 
 export function* userWatcher() {
   yield takeEvery(FETCH_USER, fetchUserWorker);
+
 }
